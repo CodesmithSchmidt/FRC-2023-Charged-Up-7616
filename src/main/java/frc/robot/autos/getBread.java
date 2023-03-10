c2051ec8b1580a0dc59c83ebe97815f529069ce3
Sebastian Rubalcava <srubalcava002@gmail.com>
2023-03-10 07:37:36 -0700

    fix auto

--- src/main/java/frc/robot/autos/getBread.java
+++ src/main/java/frc/robot/autos/getBread.java
@@ -1,8 +1,21 @@
 package frc.robot.autos;
 
+import java.util.List;
+
 import com.kauailabs.navx.frc.AHRS;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
+import frc.robot.Constants;
 import frc.robot.commands.Balance;
 import frc.robot.commands.GotoPlatform;
 import frc.robot.subsystems.NavXBalance;
@@ -14,12 +27,45 @@ public class getBread extends SequentialCommandGroup {
    private Balance balanceCommand;
 
    public getBread(Swerve swerve_in, AHRS ahrs_in) {
+    // CREATE BALANCE COMMAND
     balanceSubsystem = new NavXBalance(ahrs_in, swerve_in, null);
-
-    platformMoveCommand = new GotoPlatform(swerve_in);
     balanceCommand = new Balance(balanceSubsystem);
 
-    addCommands(platformMoveCommand, balanceCommand);
+   TrajectoryConfig config =
+   new TrajectoryConfig(
+               Constants.AutoConstants.kMaxSpeedMetersPerSecond,
+               Constants.AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+            .setKinematics(Constants.Swerve.swerveKinematics);
+
+   // An example trajectory to follow.  All units in meters.
+   Trajectory exampleTrajectory =
+      TrajectoryGenerator.generateTrajectory(
+            // Start at the origin facing the +X direction
+            new Pose2d(0, 0, new Rotation2d(0)),
+            // Pass through these two interior waypoints, making an 's' curve path
+            null,
+            // End 3 meters straight ahead of where we started, facing forward
+            new Pose2d(3, 0, new Rotation2d(0)),
+            config);
+
+   var thetaController =
+      new ProfiledPIDController(
+            Constants.AutoConstants.kPThetaController, 0, 0, Constants.AutoConstants.kThetaControllerConstraints);
+   thetaController.enableContinuousInput(-Math.PI, Math.PI);
+
+   SwerveControllerCommand swerveControllerCommand =
+      new SwerveControllerCommand(
+            exampleTrajectory,
+            swerve_in::getPose,
+            Constants.Swerve.swerveKinematics,
+            new PIDController(Constants.AutoConstants.kPXController, 0, 0),
+            new PIDController(Constants.AutoConstants.kPYController, 0, 0),
+            thetaController,
+            swerve_in::setModuleStates,
+            swerve_in);
+
+    addCommands(
+      new InstantCommand(() -> swerve_in.resetOdometry(exampleTrajectory.getInitialPose())), swerveControllerCommand, balanceCommand);
    } 
 }
 
